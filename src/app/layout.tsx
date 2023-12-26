e438864d010c2b74491493999fddd852a1ec6c66
Alsath <fiedrstar@gmail.com>
2023-12-26 19:41:36 +0300

    AlsthFixSiber

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.sass'
-import TrashLayout from './trash/layout'
-import SidebarLayout from './sidebar/layout'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.sass";
+import TrashLayout from "./trash/layout";
+import SidebarLayout from "./sidebar/layout";
+import styles from "./globalLayout.module.sass";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Bloknot',
-  description: '',
-}
+  title: "Bloknot",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head><link rel="icon" type="image/svg+xml" href="./Logo.ico" sizes="any" /></head>
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="icon" type="image/svg+xml" href="./Logo.ico" sizes="any" />
+      </head>
+      <body style={{ margin: "0" }} className={inter.className}>
+        {children}
+      </body>
       <SidebarLayout />
     </html>
-  )
+  );
 }
--- src/app/sidebar/layout.tsx
+++ src/app/sidebar/layout.tsx
@@ -1,10 +1,25 @@
-import styles from './sidebar.module.sass'
+import styles from "./sidebar.module.sass";
 
 export default function SidebarLayout() {
   return (
     <div className={styles.sidebar}>
-      <img src="/Logo.png" alt="" />
-      <ul>
+      <div className={styles.cont}>
+        <img src="/Logo.png" alt="" />
+        <ul className={styles.mainContLi}>
+          <li className={styles.MainLi}>Главная</li>
+          <li className={styles.FileLi}>Мои Файлы</li>
+          <li className={styles.StarLi}>Избранное</li>
+          <li className={styles.RequseLi}>Запрос Файлов</li>
+          <li className={styles.TrechLi}>Корзина</li>
+        </ul>
+      </div>
+      <div className={styles.underCont}></div>
+    </div>
+  );
+}
+
+{
+  /* <ul>
         <li>Главная</li>
         <li>Мои Файлы</li>
         <li>Избранное</li>
@@ -16,7 +31,5 @@ export default function SidebarLayout() {
         <span></span>
         <span></span>
         <button className={styles.btn}> Улучшить </button>
-      </div>
-    </div>
-  );
+      </div> */
 }
