5cc1a70a9aa0951fbe7f85763c9c777cc9d5cf60
Alsath <fiedrstar@gmail.com>
2023-12-27 15:08:45 +0300

    mainCard

---
+++ src/app/components/fileCard/Card.tsx
@@ -0,0 +1,5 @@
+import styles from "./Card.module.sass";
+
+export const CardComp = () => {
+  return <div className={styles.CardCont}></div>;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import styles from "./globalLayout.module.sass";
+import { CardComp } from "./components/fileCard/Card";
 export const metadata: Metadata = {
   title: "Bloknot",
 };
@@ -7,7 +8,23 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className={styles.MainCont}>
-      <div className={styles.topBlock}></div>
+      <div className={styles.topBlock}>
+        <div className={styles.Text}>
+          <div className={styles.textOne}>Все файлы</div>
+          <div className={styles.textCont}>
+            <div className={styles.textTwo}>Добро пожаловать, Михаил! 👋 </div>
+          </div>
+          <button className={styles.btn}>+ Добавить </button>
+        </div>
+        <div className={styles.CardArr}>
+          <CardComp />
+          <CardComp />
+          <CardComp />
+          <CardComp />
+          <CardComp />
+          <CardComp />
+        </div>
+      </div>
       <div className={styles.underBlock}></div>
     </div>
   );
