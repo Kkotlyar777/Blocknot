565556a563201577d51b5f628a3dc86dd10331e5
L1MBO <kotlark80@gmail.com>
2024-01-25 15:30:57 +0100

    123

--- src/app/GlobalRedux/reducer/provider.tsx
+++ src/app/GlobalRedux/reducer/provider.tsx
@@ -3,7 +3,7 @@
 import { Provider } from "react-redux";
 import { setupStore } from "../store";
 
-export const Provid = ({ children }) => {
+export const Provid = ({ children }) : JSX.Element  => {
   const store = setupStore();
   return <Provider store={store}>{children}</Provider>;
 };
