cf146d97df31c66c313aabb089f7dadc126acd31
Alsath <fiedrstar@gmail.com>
2024-02-25 16:35:55 +0300

    fix3

--- src/features/ButtonMain/button.tsx
+++ src/features/ButtonMain/button.tsx
@@ -1,20 +1,20 @@
-"use client";
-import styles from "./Button.module.sass";
-import { ArrSlice } from "@/app/GlobalRedux/reducer/ArrSlice";
-import { useAppDispatch, useAppSelector } from "@/features/hooks/redux";
+'use client';
+import styles from './Button.module.sass';
+import { ArrSlice } from '@/app/GlobalRedux/reducer/ArrSlice';
+import { useAppDispatch, useAppSelector } from '@/features/hooks/redux';
 
 export const ButtonMain = () => {
-  // получение actions
-  const { AddCard } = ArrSlice.actions;
-  const dispatch = useAppDispatch();
+	// получение actions
+	const { AddCard } = ArrSlice.actions;
+	const dispatch = useAppDispatch();
 
-  return (
-    <button
-      className={styles.main}
-      // создание новой карточки
-      onClick={() => dispatch(AddCard())}
-    >
-      + Добавить
-    </button>
-  );
+	return (
+		<button
+			className={styles.main}
+			// создание новой карточки
+			onClick={() => dispatch(AddCard('12'))}
+		>
+			+ Добавить
+		</button>
+	);
 };
